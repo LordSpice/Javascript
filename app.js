083bb1135176d6ca70669e617fab1c6afac38909
Joseph <47985330+LordSpice@users.noreply.github.com>
2022-10-15 02:29:03 +0300

    Added compare methods

--- app.js
+++ app.js
@@ -11,16 +11,20 @@
         this.fact = fact;
         this.image = image;
             
-    }
-};
+        }
+    };
 
     // Create Dino Objects
     let dinos = [];
     fetch('./dino.json')
         .then(result => result.json())
         .then(data => data.Dinos
-            .forEach(dino => dinos.push(new Dino(dino.species, dino.weight, dino.height, dino.diet, dino.when, dino.fact, './images/' + dino.species + '.png')))
-            );
+            .forEach(
+                dino => dinos.push(new Dino(dino.species, dino.weight, dino.height, dino.diet, dino.when, dino.fact, './images/' + dino.species + '.png'))
+            )
+        );
+
+    // console.log(dinos);
     
     // dinos.forEach(dino => {
     //     species = dino.species;
@@ -43,34 +47,52 @@
         weight: '',
         diet: '',
         image: './images/human.png',
-    }
+    };
 
     // Use IIFE to get human data from form
-    document.getElementById('btn').addEventListener('click', getHuman)
+    document.getElementById('btn').addEventListener('click', getHuman);
 
     function getHuman() {
-        human.name = document.getElementById('name').innerText;
-        //human.height = (parseInt(document.getElementById('heightfeet').innerHTML) * 12) + parseInt(document.getElementById('heightinches').innerHTML);
-        human.weight = document.getElementById('weight').innerText;
-        human.diet = document.getElementById('diet').innerText;
-            
-        console.log(dinos);
+        human.name = document.getElementById('name').value;
+        human.height = (parseInt(document.getElementById('feet').value) * 12) + parseInt(document.getElementById('inches').value);
+        human.weight = document.getElementById('weight').value;
+        human.diet = document.getElementById('diet').value;
+           
         console.log(human);
     }
+    
     // Create Dino Compare Method 1
     // NOTE: Weight in JSON file is in lbs, height in inches. 
-
+    function compareWeight(dino) {
+        if (dino.weight == human.weight) {
+            return 'You weigh the same as a ' + dino.name;
+        } else {
+            return dino.weight < human.weight ? 'You weigh less than a ' + dino.name : 'You weight more than a ' + dino.name;
+        }
+    }
     
     // Create Dino Compare Method 2
     // NOTE: Weight in JSON file is in lbs, height in inches.
-
+    function compareHeight(dino) {
+        if (dino.height == human.height) {
+            return 'Your height is the same as a ' + dino.name;
+        } else {
+            return dino.height < human.height ? 'Your height is less than a ' + dino.name : 'Your height is more than a ' + dino.name;
+        }
+    }
     
     // Create Dino Compare Method 3
     // NOTE: Weight in JSON file is in lbs, height in inches.
-
+    function compareName(dino) {
+        if (dino.name.length == human.name.length) {
+            return 'Your name is as long as the name of a ' + dino.name;
+        } else {
+            return dino.name.length < human.name.length ? 'Your name is shorter than the name of a ' + dino.name : 'Your name is longer than the name of a ' + dino.name;
+        }
+    }
 
     // Generate Tiles for each Dino in Array
-    document.getElementById('grid')
+    const grid = document.getElementById('grid');
         // Add tiles to DOM
 
     // Remove form from screen
